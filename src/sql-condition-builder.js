7b6857eb4585b6e4c74b33b64f48d28bdf663b45
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2021-03-09 08:38:47 +0100

    Fix parsing text with quotes in brackets

--- src/sql-condition-builder.js
+++ src/sql-condition-builder.js
@@ -1,150 +1,166 @@
-import squel from 'squel'
-
+import squel from "squel";
 
 export default class SQLConditionBuilder {
   constructor() {
-    this.valueFormatters = []
+    this.valueFormatters = [];
 
-    this.registerValueFormatter(null, value => {  // eslint-disable-line no-unused-vars
-      return 'IS NULL'
-    })
-    this.registerValueFormatter('null', value => {  // eslint-disable-line no-unused-vars
-      return 'IS NULL'
-    })
-    this.registerValueFormatter('!null', value => {  // eslint-disable-line no-unused-vars
-      return 'IS NOT NULL'
-    })
-    this.registerValueFormatter('>=', value => {
-      return `>= ${this._evalAndEscapeValue(value.substring(2))}`
-    })
-    this.registerValueFormatter('>', value => {
-      return `> ${this._evalAndEscapeValue(value.substring(1))}`
-    })
-    this.registerValueFormatter('<=', value => {
-      return `<= ${this._evalAndEscapeValue(value.substring(2))}`
-    })
-    this.registerValueFormatter('<', value => {
-      return `< ${this._evalAndEscapeValue(value.substring(1))}`
-    })
-    this.registerValueFormatter('!', value => {
-      return `<> ${this._evalAndEscapeValue(value.substring(1))}`
-    })
-    this.registerValueFormatter(/[\*\?]+/, value => {
-      return `LIKE ${this._evalAndEscapeValue(value.replace(/\*/g, '%').replace(/\?/, '_'))}`
-    })
-    this.registerValueFormatter(/^\[.+ TO .+\]$/, value => {
+    this.registerValueFormatter(null, (value) => {
+      // eslint-disable-line no-unused-vars
+      return "IS NULL";
+    });
+    this.registerValueFormatter("null", (value) => {
+      // eslint-disable-line no-unused-vars
+      return "IS NULL";
+    });
+    this.registerValueFormatter("!null", (value) => {
+      // eslint-disable-line no-unused-vars
+      return "IS NOT NULL";
+    });
+    this.registerValueFormatter(">=", (value) => {
+      return `>= ${this._evalAndEscapeValue(value.substring(2))}`;
+    });
+    this.registerValueFormatter(">", (value) => {
+      return `> ${this._evalAndEscapeValue(value.substring(1))}`;
+    });
+    this.registerValueFormatter("<=", (value) => {
+      return `<= ${this._evalAndEscapeValue(value.substring(2))}`;
+    });
+    this.registerValueFormatter("<", (value) => {
+      return `< ${this._evalAndEscapeValue(value.substring(1))}`;
+    });
+    this.registerValueFormatter("!", (value) => {
+      return `<> ${this._evalAndEscapeValue(value.substring(1))}`;
+    });
+    this.registerValueFormatter(/[\*\?]+/, (value) => {
+      return `LIKE ${this._evalAndEscapeValue(
+        value.replace(/\*/g, "%").replace(/\?/, "_")
+      )}`;
+    });
+    this.registerValueFormatter(/^\[.+ TO .+\]$/, (value) => {
       const splitted = value
         .substring(1, value.length - 1)
-        .split(' TO ')
-        .map(item => this._evalAndEscapeValue(item))
+        .split(" TO ")
+        .map((item) => this._evalAndEscapeValue(item));
 
-      const [fromValue, toValue] = splitted
+      const [fromValue, toValue] = splitted;
 
-      return `BETWEEN ${fromValue} AND ${toValue}`
-    })
-    this.registerValueFormatter(/^\[(.+,)*.+\]$/, value => {
+      return `BETWEEN ${fromValue} AND ${toValue}`;
+    });
+    this.registerValueFormatter(/^\[(.+,)*.+\]$/, (value) => {
       const values = value
         .substring(1, value.length - 1)
-        .split(',')
-        .map(item => item.trim())
-        .map(item => this._evalAndEscapeValue(item))
-        .join(', ')
+        .split(",")
+        .map((item) => item.trim())
+        .map((item) => this._evalAndEscapeValue(item))
+        .join(", ");
 
-      return `IN (${values})`
-    })
+      return `IN (${values})`;
+    });
   }
 
   build(object) {
-    return this.getExpression(object).toString()
+    return this.getExpression(object).toString();
   }
 
   getExpression(objectOrArray) {
-    const expr = squel.expr()
+    const expr = squel.expr();
 
     if (Array.isArray(objectOrArray)) {
-      this._buildExpressionWithArray(expr, objectOrArray)
+      this._buildExpressionWithArray(expr, objectOrArray);
     } else {
-      this._buildExpressionWithObject(expr, objectOrArray)
+      this._buildExpressionWithObject(expr, objectOrArray);
     }
 
-    return expr
+    return expr;
   }
-
 
   _buildExpressionWithArray(expr, array) {
     return Array.from(array).map((value) =>
-      value instanceof Object ?
-        expr.or(this.build(value))
-        :
-        expr.or(value))
+      value instanceof Object ? expr.or(this.build(value)) : expr.or(value)
+    );
   }
 
   _buildExpressionWithObject(expr, object) {
     return (() => {
-      const result = []
+      const result = [];
 
       for (const key in object) {
-        const value = object[key]
+        const value = object[key];
 
         if (value instanceof Object) {
-          result.push(expr.and(`(${this.build(value)})`))
+          result.push(expr.and(`(${this.build(value)})`));
         } else {
-          const parsedValue = this._parseValue(value)
+          const parsedValue = this._parseValue(value);
 
           if (parsedValue) {
-            result.push(expr.and(`${key} ${parsedValue}`))
+            result.push(expr.and(`${key} ${parsedValue}`));
           } else {
-            result.push(expr.and(`${key} = ${this._escapeValue(value)}`))
+            result.push(expr.and(`${key} = ${this._escapeValue(value)}`));
           }
         }
       }
 
-      return result
-    })()
+      return result;
+    })();
   }
 
   _parseValue(value) {
     for (const f of Array.from(this.valueFormatters)) {
       if (f.format instanceof RegExp && f.format.test(value)) {
-        return f.fn(value)
-      } else if ((value === f.format) || (value && ((typeof value.indexOf === 'function' ? value.indexOf(f.format) : undefined) === 0))) {
-        return f.fn(value)
+        return f.fn(value);
+      } else if (
+        value === f.format ||
+        (value &&
+          (typeof value.indexOf === "function"
+            ? value.indexOf(f.format)
+            : undefined) === 0)
+      ) {
+        return f.fn(value);
       }
     }
-    return null
+    return null;
   }
 
   registerValueFormatter(formatOrPrefix, formatterFunction) {
-    return this.valueFormatters.push({ format: formatOrPrefix, fn: formatterFunction })
+    return this.valueFormatters.push({
+      format: formatOrPrefix,
+      fn: formatterFunction,
+    });
   }
 
   _escapeValue(value) {
-    if (typeof value !== 'string') {
-      return value
+    if (typeof value !== "string") {
+      return value;
     }
 
-    return this._wrapStringValue(value.replace(/\'/g, '\\\''))
+    if (
+      (value[0] === "'" && value[value.length - 1] === "'") ||
+      (value[0] === `"` && value[value.length - 1] === `"`)
+    ) {
+      value = value.substring(1, value.length - 1);
+    }
+    return this._wrapStringValue(value.replace(/\'/g, "\\'"));
   }
 
   _evalAndEscapeValue(value) {
     // Return value as is if it's a valid number
     if (!isNaN(value)) {
-      return value
+      return value;
     }
 
     // Strings can be enclosed by double quotes,
     // parse them for correctness or keep them as is if not correctly wrapper by duoble quotes
     try {
-      value = JSON.parse(value)
+      value = JSON.parse(value);
     } catch (e) {
       // eslint disable-line no-empty
     }
 
     // Return string encoded and wrapped in single quotes
-    return this._escapeValue(value)
+    return this._escapeValue(value);
   }
 
   _wrapStringValue(value) {
-    return value[0] === '`' ? value : `'${value}'`
+    return value[0] === "`" ? value : `'${value}'`;
   }
 }
--- test.js
+++ test.js
@@ -1,117 +1,176 @@
-import assert from 'assert'
+import SQLConditionBuilder from "./src/sql-condition-builder";
+import assert from "assert";
 
-import SQLConditionBuilder from './src/sql-condition-builder'
-
-
-const builder = new SQLConditionBuilder()
+const builder = new SQLConditionBuilder();
 
 describe("plain content", () => {
   it("should generate correct condition from object", () => {
-    const obj = { test: "xxx", hello: "wor'ld", number: 25 }
-    const cond = builder.build(obj)
+    const obj = { test: "xxx", hello: "wor'ld", number: 25 };
+    const cond = builder.build(obj);
 
-    assert.equal(cond, "test = 'xxx' AND hello = 'wor\\'ld' AND number = 25")
-  })
+    assert.equal(cond, "test = 'xxx' AND hello = 'wor\\'ld' AND number = 25");
+  });
 
   it("should generate correct condition from array", () => {
-    const arr = ["test='xxx'", "hello='world'", "number=25", "value=`value`"]
-    const cond = builder.build(arr)
+    const arr = ["test='xxx'", "hello='world'", "number=25", "value=`value`"];
+    const cond = builder.build(arr);
 
-    assert.equal(cond, "test='xxx' OR hello='world' OR number=25 OR value=`value`")
-  })
-})
+    assert.equal(
+      cond,
+      "test='xxx' OR hello='world' OR number=25 OR value=`value`"
+    );
+  });
+});
 
 describe("nested content", () => {
   it("should generate correct condition from nested objects/arrays", () => {
-    const obj = { ignoredKey: [{ aa: "aa", bb: "bb" }, { xx: "yy", yy: "xx" }], test: 125 }
-    const arr = [{ ignoredKey: [{ aa: "aa", bb: "bb" }, { xx: "yy", yy: "xx" }] }, { test: 125 }]
-    const cond = builder.build(obj)
-    const cond2 = builder.build(arr)
+    const obj = {
+      ignoredKey: [
+        { aa: "aa", bb: "bb" },
+        { xx: "yy", yy: "xx" },
+      ],
+      test: 125,
+    };
+    const arr = [
+      {
+        ignoredKey: [
+          { aa: "aa", bb: "bb" },
+          { xx: "yy", yy: "xx" },
+        ],
+      },
+      { test: 125 },
+    ];
+    const cond = builder.build(obj);
+    const cond2 = builder.build(arr);
 
-    assert.equal(cond, "(aa = 'aa' AND bb = 'bb' OR xx = 'yy' AND yy = 'xx') AND test = 125")
-    assert.equal(cond2, "(aa = 'aa' AND bb = 'bb' OR xx = 'yy' AND yy = 'xx') OR test = 125")
-  })
-})
+    assert.equal(
+      cond,
+      "(aa = 'aa' AND bb = 'bb' OR xx = 'yy' AND yy = 'xx') AND test = 125"
+    );
+    assert.equal(
+      cond2,
+      "(aa = 'aa' AND bb = 'bb' OR xx = 'yy' AND yy = 'xx') OR test = 125"
+    );
+  });
+});
 
 describe("value parsers", () => {
   it("should parse in", () => {
-    const obj = { inNumber: '[1, 2]', inString: '[a, b]', inQuoted: '["1", "2"]' }
-    const cond = builder.build(obj)
+    const obj = {
+      inNumber: "[1, 2]",
+      inString: "[a, b]",
+      inQuoted: '["1", "2"]',
+    };
+    const cond = builder.build(obj);
 
-    assert.equal(cond, "inNumber IN (1, 2) AND inString IN ('a', 'b') AND inQuoted IN ('1', '2')")
-  })
+    assert.equal(
+      cond,
+      "inNumber IN (1, 2) AND inString IN ('a', 'b') AND inQuoted IN ('1', '2')"
+    );
+  });
   it("should not parse in for text brackets", () => {
     const obj = { inString: "[a, b]", notInString: "prefix [text] sufix" };
     const cond = builder.build(obj);
 
-    assert.equal(cond, "inString IN ('a', 'b') AND notInString = 'prefix [text] sufix'");
+    assert.equal(
+      cond,
+      "inString IN ('a', 'b') AND notInString = 'prefix [text] sufix'"
+    );
+  });
+  it("should parse text brackets with quotes", () => {
+    const obj = { inString: "['a', 'b']" };
+    const cond = builder.build(obj);
+
+    assert.equal(cond, "inString IN ('a', 'b')");
+  });
+  it("should parse text brackets with double quotes", () => {
+    const obj = { inString: `["a", "b"]` };
+    const cond = builder.build(obj);
+
+    assert.equal(cond, "inString IN ('a', 'b')");
   });
 
   it("should parse between", () => {
-    const obj = { btNumber: '[10 TO 1000]', btString: '[a TO z]', btQuoted: '["1" TO "10"]' }
-    const cond = builder.build(obj)
+    const obj = {
+      btNumber: "[10 TO 1000]",
+      btString: "[a TO z]",
+      btQuoted: '["1" TO "10"]',
+    };
+    const cond = builder.build(obj);
 
     assert.equal(
       cond,
       "btNumber BETWEEN 10 AND 1000 AND btString BETWEEN 'a' AND 'z' AND btQuoted BETWEEN '1' AND '10'"
-    )
-  })
+    );
+  });
 
   it("should parse like", () => {
-    const obj = { like: 'li*k?' }
-    const cond = builder.build(obj)
+    const obj = { like: "li*k?" };
+    const cond = builder.build(obj);
 
-    assert.equal(cond, "like LIKE 'li%k_'")
-  })
+    assert.equal(cond, "like LIKE 'li%k_'");
+  });
 
   it("should parse not equal", () => {
-    const obj = { neNumber: '!25', neString: '!aa', neQuoted: '!"25"' }
-    const cond = builder.build(obj)
+    const obj = { neNumber: "!25", neString: "!aa", neQuoted: '!"25"' };
+    const cond = builder.build(obj);
 
-    assert.equal(cond, "neNumber <> 25 AND neString <> 'aa' AND neQuoted <> '25'")
-  })
+    assert.equal(
+      cond,
+      "neNumber <> 25 AND neString <> 'aa' AND neQuoted <> '25'"
+    );
+  });
 
   it("should parse equal", () => {
-    const obj = { eqNumber: 25, eqStrNumber: '25', eqString: 'aa' }
-    const cond = builder.build(obj)
+    const obj = { eqNumber: 25, eqStrNumber: "25", eqString: "aa" };
+    const cond = builder.build(obj);
 
-    assert.equal(cond, "eqNumber = 25 AND eqStrNumber = '25' AND eqString = 'aa'")
-  })
+    assert.equal(
+      cond,
+      "eqNumber = 25 AND eqStrNumber = '25' AND eqString = 'aa'"
+    );
+  });
 
   it("should parse greater or equal than", () => {
-    const obj = { geNumber: '>=25', geString: '>=aa', geQuoted: '>="25"' }
-    const cond = builder.build(obj)
+    const obj = { geNumber: ">=25", geString: ">=aa", geQuoted: '>="25"' };
+    const cond = builder.build(obj);
 
-    assert.equal(cond, "geNumber >= 25 AND geString >= 'aa' AND geQuoted >= '25'")
-  })
+    assert.equal(
+      cond,
+      "geNumber >= 25 AND geString >= 'aa' AND geQuoted >= '25'"
+    );
+  });
 
   it("should parse greater than", () => {
-    const obj = { gtNumber: '>25', gtString: '>aa', gtQuoted: '>"25"' }
-    const cond = builder.build(obj)
+    const obj = { gtNumber: ">25", gtString: ">aa", gtQuoted: '>"25"' };
+    const cond = builder.build(obj);
 
-    assert.equal(cond, "gtNumber > 25 AND gtString > 'aa' AND gtQuoted > '25'")
-  })
+    assert.equal(cond, "gtNumber > 25 AND gtString > 'aa' AND gtQuoted > '25'");
+  });
 
   it("should parse less or equal than", () => {
-    const obj = { leNumber: '<=25', leString: '<=aa', leQuoted: '<="25"' }
-    const cond = builder.build(obj)
+    const obj = { leNumber: "<=25", leString: "<=aa", leQuoted: '<="25"' };
+    const cond = builder.build(obj);
 
-    assert.equal(cond, "leNumber <= 25 AND leString <= 'aa' AND leQuoted <= '25'")
-  })
+    assert.equal(
+      cond,
+      "leNumber <= 25 AND leString <= 'aa' AND leQuoted <= '25'"
+    );
+  });
 
   it("should parse less than", () => {
-    const obj = { ltNumber: '<25', ltString: '<aa', ltQuoted: '<"25"' }
-    const cond = builder.build(obj)
+    const obj = { ltNumber: "<25", ltString: "<aa", ltQuoted: '<"25"' };
+    const cond = builder.build(obj);
 
-    assert.equal(cond, "ltNumber < 25 AND ltString < 'aa' AND ltQuoted < '25'")
-  })
-})
+    assert.equal(cond, "ltNumber < 25 AND ltString < 'aa' AND ltQuoted < '25'");
+  });
+});
 
 describe("null values", () => {
   it("should recognize null comparisons", () => {
-    const obj = { a: null, b: "null", c: "!null" }
-    const cond = builder.build(obj)
+    const obj = { a: null, b: "null", c: "!null" };
+    const cond = builder.build(obj);
 
-    assert.equal(cond, "a IS NULL AND b IS NULL AND c IS NOT NULL")
-  })
-})
+    assert.equal(cond, "a IS NULL AND b IS NULL AND c IS NOT NULL");
+  });
+});
